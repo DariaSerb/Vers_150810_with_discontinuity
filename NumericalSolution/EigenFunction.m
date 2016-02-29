e75c42563f0c80d43d986d22d8f4ef0f9b3030f3
Daria <dariya.serb@gmail.com>
2016-02-29 23:58:57 +0200

    Update EigenFunction.m

--- NumericalSolution/EigenFunction.m
+++ NumericalSolution/EigenFunction.m
@@ -1,10 +1,20 @@
-function [X,u] = EigenFunction(dTau)
+function [X,uest] = EigenFunction(dTau)
 global P;
 P = Parameters;
 
 alp = AlpShapes; 
 u = InitialApproximation;
-u = u + alp*u*dTau;
+Du = zeros(P.ModeEst,P.PointCnt);
+uest = zeros(P.ModeEst,P.PointCnt);
+
+for i = 1:P.ModeEst
+  for k = i+1:P.ModeCnt
+    for n = 1:P.PointCnt;
+         Du(i,n) = Du(i,n) + alp(i,k)*u(k,n);
+    end
+  end
+end
+uest = u(1:P.ModeEst,:) + Du*dTau;
 
 X = P.X;
 [q,qs] = q_calc(X);
@@ -14,5 +24,6 @@ end
 function u = InitialApproximation
 global P;
 % u = zeros(P.ModeCnt, P.PointCnt);
-[lambda, u] = eig_val(P); 
+% [lambda, u] = eig_val(P); 
+[lambda, u, V, stiffdq, massdq, mui] = eig_val(P); 
 end
