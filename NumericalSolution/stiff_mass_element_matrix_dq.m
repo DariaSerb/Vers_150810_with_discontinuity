d22f421893cd2d31837391c686eb95d666b7f43e
Daria <dariya.serb@gmail.com>
2016-02-29 11:57:11 +0100

    Update stiff_mass_element_matrix_dq.m
    numerical integration for elementary matrices

--- NumericalSolution/stiff_mass_element_matrix_dq.m
+++ NumericalSolution/stiff_mass_element_matrix_dq.m
@@ -1,31 +1,37 @@
-function [P0dq, K0dq] = stiff_mass_element_matrix_dq(xle,A1,A2)
+ffunction [P0dq, K0dq] = stiff_mass_element_matrix_dq(xn,xle,A1,A2)
 
 % definition size of mass matrix and stiffness matrix
-xmatrixmass=zeros(2,2);
-xmatrixk=zeros(2,2);
-npg=5;
-[x,w]=cal_point_gauss(npg);
-d=xle;
+xmatrixmassdq = zeros(2,2);
+xmatrixkdq = zeros(2,2);
+npg = 5;
+
+q = zeros(npg,1); 
+qs = zeros(npg,1); 
+
+[x,w] = cal_point_gauss(npg);
+d = xn(2) - xn(1);
 % numerical integration (0 - xle)
- for ig=1:npg
-    s=(d/2)*(x(ig)+1);
-    N2=s/xle;
-    N1=1-N2;
-    [q,qs] = q_calc(s);
+ for ig = 1:npg
+    s = (d/2)*(x(ig)+1);
+    s1 = s + xn(1);
     
-    N=[N1, N2];
-    xmatrixmass=xmatrixmass+A2*(N'*N)*w(ig)*qs(ig);
+    N2 = s/xle;
+    N1 = 1-N2;
+    [q(ig),qs(ig)] = q_calc(s1);
     
-    dN(1)=-1/xle;
-    dN(2)=1/xle;
-    [q,qs] = q_calc(s);
+    N = [N1, N2];
+    xmatrixmassdq = xmatrixmassdq + A2*(N'*N)*w(ig)*qs(ig);
     
-    dN=[dN(1), dN(2)];
-    xmatrixk=xmatrixk+A1*(dN'*dN)*w(ig)*qs(ig);
+    dN(1) = -1/xle;
+    dN(2) = 1/xle;
+    [q(ig),qs(ig)] = q_calc(s1);
+    
+    dN = [dN(1), dN(2)];
+    xmatrixkdq = xmatrixkdq + A1*(dN'*dN)*w(ig)*qs(ig);
  end
-    xmatrixmass=xmatrixmass*(d/2);
-    xmatrixk=xmatrixk*(d/2);
+    xmatrixmassdq = xmatrixmassdq*(d/2);
+    xmatrixkdq = xmatrixkdq*(d/2);
     
-    P0dq = xmatrixmass;
-    K0dq = xmatrixk;
+    P0dq = xmatrixmassdq;
+    K0dq = xmatrixkdq;
 end
